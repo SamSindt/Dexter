860a81ec555abe719b93435a0a3ac0e9815f9c7a
SamSindt <samsground@gmail.com>
2019-11-22 10:21:05 -0800

    Added types to showPokemon and created link to styles.

---
+++ PHP/queryType.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('basicErrorHandling.php');
+
+	function getTypeImageIDByPokemon($dbh, $pokemonID)
+	{
+		$rows = array();
+
+		$sth = $dbh -> prepare("SELECT TypeImageID
+                                FROM HasTypes, Types
+                                WHERE PokemonID = " . $pokemonID . " AND HasTypes.TypeID = Types.TypeID" );
+		// run the query
+		$sth -> execute();
+		
+		while ($row = $sth -> fetch ()) {
+			$rows[] = $row;
+		}
+		return $rows;
+	}
+?>	
