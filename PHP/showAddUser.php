be0d19a8d0e89628ba8bd473c4ca28233687f2b2
SamSindt <samsground@gmail.com>
2019-11-24 12:59:19 -0800

    Added styles to showAddUser

--- PHP/showAddUser.php
+++ PHP/showAddUser.php
@@ -1,37 +1,35 @@
-<?php
-
- 	/*require_once('basicErrorHandling.php');
-	require_once ('connDB.php');
-	
-	session_start();
-	$dbh = db_connect_ro();*/
-?>
-
 <!doctype html>
 <html>
 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href="navigationStyles.css" rel="stylesheet">
+		<link href="showLoginStyles.css" rel="stylesheet">
+		<link href="showAddUserStyles.css" rel="stylesheet">
 		<title>Create an Account</title>
 	</head>
 	
 	<body>
-	<h1>Create an Account</h1>
-		
-		<form method="post" action="addUser.php">
-		
-		Username:
-		<input name="user" size="15" type="text" ><br>
-		Password:
-		<input name="pass" size="15" type="password" ><br>
-		
-		<input TYPE="submit" NAME="btnRegister" VALUE="Register">
-		
-		</form>
+	<div class="topBar">
+		<div class="title"><b>Pokedex</b></div>
+			<ul class="nav">
+				<li class="navItem"><a href="showPokemonSearch.php">Home</a></li>
+			</ul>
+		</div>
+	</div>
+		<h1>Create an Account</h1>
+		<div class="wrapper">
+			<div class="inner">
+				<form method="post" action="addUser.php">
+					
+					<div class="item">Username:<input name="user" size="15" type="text" ><br></div>
+					<div class="item">Password:<input name="pass" size="15" type="password" ><br></div>
+					
+					<input class="item" TYPE="submit" NAME="btnRegister" VALUE="Register">
+				
+				</form>
+			</div>
+		</div>
 	</body>
 
 </html>
-
-<?php
-	//db_close($dbh);
-?>
