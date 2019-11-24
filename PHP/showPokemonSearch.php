b485a703dc9c23667c3a6abc67d90d68bb15904b
SamSindt <samsground@gmail.com>
2019-11-23 23:16:19 -0800

    Added styles to showPokemonSeearch and cleaned up some of the html

--- PHP/showPokemonSearch.php
+++ PHP/showPokemonSearch.php
@@ -5,102 +5,111 @@
 	
 	session_start();
 
-	 $dbh = db_connect_ro();
+	$dbh = db_connect_ro();
 ?>
 
-<html>
+<!doctype html>
+<html lang="en-us">
 
 	<head>
-		<title></title>
+		<meta charset="utf-8">
+		<title>Home</title>
+		<link href="navigationStyles.css" rel="stylesheet">
+		<link href="showPokemonSearchStyles.css" rel="stylesheet">
 	</head>
 
 	<body>
 	
-	<div class="topBar">
-		<div class="title"><b>Pokedex</b></div>
-			<ul class="nav">
-				<li class="navItem"><a href="showPokemonSearch.php">Home</a></li>
-				<li class="navItem"><a>Team</a></li>
-				<li class="navItem"><a>Favorites</a></li>
-				<li class="navItem"><a>Login</a></li>
-			</ul>
+		<div class="topBar">
+			<div class="title"><b>Pokedex</b></div>
+				<ul class="nav">
+					<li class="navItem"><a href="showPokemonSearch.php">Home</a></li>
+					<li class="navItem"><a>Team</a></li>
+					<li class="navItem"><a>Favorites</a></li>
+					<li class="navItem"><a>Login</a></li>
+				</ul>
+			</div>
 		</div>
-	</div>
-	
-	<form method="post" action="showResultsPage.php">
-
-	Name:
-	<input TYPE="search" NAME="NameLike">
-	<p>
-
-	ID:
-	<input TYPE="number" NAME="PKID"
-		<?php
-			print " MIN=\"" . getLowestPKID($dbh) . "\" MAX=\"" . getHighestPKID($dbh) . "\">";
-		?>
-	<p>
-	
-	Type:
-	<select NAME="TypeID">
-		<option VALUE=0></option>
-		<?php
-			$data = getTypes($dbh);
-			
-			foreach ( $data as $row )
-			{
-  			print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
-			}
-		?>
-	</select>
-	<p>
-	
-	Analog:
-	<select NAME="AnalogID">
-		<option VALUE=0></option>
-		<?php
-			$data = getAnalogs($dbh);
-			
-			foreach ( $data as $row )
-			{
-  			print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
-			}
-		?>
-	</select>
-	<p>
-	
-	Color:
-	<select NAME="ColorID">
-		<option VALUE=0></option>
-		<?php
-			$data = getColors($dbh);
-			
-			foreach ( $data as $row )
-			{
-  			print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
-			}
-		?>
-	</select>
-	<p>
-	
-	Egg Group:
-	<select NAME="EggGroupID">
-		<option VALUE=0></option>
-		<?php
-			$data = getEggGroups($dbh);
-			
-			foreach ( $data as $row )
-			{
-  			print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
-			}
-		?>
-	</select>
-	<p>
-	
-	<input TYPE="submit" NAME="Request" VALUE="Search" />
 		
-	</body>
-</form>
+		<h1>Search Pokemon</h1>
+	
+		<div class="wrapper">
+	
+		<form method="post" action="showResultsPage.php">
+		
+		<div class="formField">Name:
+			<input TYPE="search" NAME="NameLike">
+		</div>
+		
 
+			<div class="formField">ID:
+				<input TYPE="number" NAME="PKID"
+					<?php
+						print " MIN=\"" . getLowestPKID($dbh) . "	\" MAX=\"" . getHighestPKID($dbh) . "\">";
+					?>
+			</div>
+			
+			<div class="formField">Type:
+				<select NAME="TypeID">
+					<option VALUE=0></option>
+					<?php
+						$data = getTypes($dbh);
+						
+						foreach ( $data as $row )
+						{
+							print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
+						}
+					?>
+				</select>
+			</div>
+
+			<div class="formField">Analog:
+				<select NAME="AnalogID">
+					<option VALUE=0></option>
+					<?php
+						$data = getAnalogs($dbh);
+						
+						foreach ( $data as $row )
+						{
+							print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
+						}
+					?>
+				</select>
+			</div>
+
+			
+			<div class="formField">Color:
+				<select NAME="ColorID">
+					<option VALUE=0></option>
+					<?php
+						$data = getColors($dbh);
+						
+						foreach ( $data as $row )
+						{
+						print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
+						}
+					?>
+				</select>
+			</div>
+			
+			<div class="formField">Egg Group:
+				<select NAME="EggGroupID">
+					<option VALUE=0></option>
+					<?php
+						$data = getEggGroups($dbh);
+						
+						foreach ( $data as $row )
+						{
+						print '<option VALUE=' . $row[0] . '> ' . $row[1] . '</option>';
+						}
+					?>
+				</select>
+			</div>
+			
+				<input id="search" class="formField" TYPE="submit" NAME="Request" VALUE="Search">
+			</form>
+		</div>
+	</body>
 </html>
 
 <?php
