fde8b976917c439090a1be88c660301a03eff370
SamSindt <samsground@gmail.com>
2019-11-23 17:47:17 -0800

    Added nav and scroll wheel to showResultsPage

--- PHP/showResultsPage.php
+++ PHP/showResultsPage.php
@@ -13,20 +13,33 @@
 <html lang="en-us">
     <head>
 		<meta charset="utf-8">
-		<link href="showPokemonStyles.css" rel="stylesheet">
+		<link href="showResultsPageStyles.css" rel="stylesheet">
 		<title>Search Results</title>
 	</head>
 		
 	<body>
-		<ul>
-			<?php
-				$searchData = searchPokemon($dbh, $_POST);
-				
-				foreach ($searchData as $data) {
-					print "<li><a href='showPokemon.php?pkid=" . $data['PKID'] . "'>" . $data['Name'] . "</a></li>";
-				}
-			?>
-		</ul>
+		<div class="topBar">
+			<div class="title"><b>Pokedex</b></div>
+			<ul class="nav">
+				<li class="navItem"><a>Home</a></li>
+				<li class="navItem"><a>Search</a></li>
+				<li class="navItem"><a>Team</a></li>
+				<li class="navItem"><a>Favorites</a></li>
+				<li class="navItem"><a>Login</a></li>
+			</ul>
+		</div>
+		
+		<div class="results">
+			<ul>
+				<?php
+					$searchData = searchPokemon($dbh, $_POST);
+					
+					foreach ($searchData as $data) {
+						print "<li><a href='showPokemon.php?pkid=" . $data['PKID'] . "'>" . $data['Name'] . "</a></li>";
+					}
+				?>
+			</ul>
+		</div>
 	</body>
 </html>
 
