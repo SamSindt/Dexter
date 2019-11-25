f2d77b4d6d54d92b334d014254f2be82a1c148a1
SamSindt <samsground@gmail.com>
2019-11-25 00:03:14 -0800

    Added async unfavoriting from favorites page

--- PHP/favoriting.js
+++ PHP/favoriting.js
@@ -2,7 +2,7 @@ function favorite (element, pkid) {
 	var newEl = document.createElement("h1");
 	newEl.setAttribute ("class", "fave");
 	newEl.setAttribute ("id", "faved");
-	newEl.setAttribute ("onclick", "unfavorite(this, ${pkid})");
+	newEl.setAttribute ("onclick", "unfavorite(this, " + String(pkid) + ")");
 	newEl.innerHTML = "★";
 	element.parentNode.replaceChild(newEl, element);
 	
@@ -19,9 +19,23 @@ function unfavorite (element, pkid) {
 	var newEl = document.createElement("h1");
 	newEl.setAttribute ("class", "fave");
 	newEl.setAttribute ("id", "unfaved");
-	newEl.setAttribute ("onclick", "favorite(this, ${pkid})");
+	newEl.setAttribute ("onclick", "favorite(this, " + String(pkid) + ")");
 	newEl.innerHTML = "☆";
 	element.parentNode.replaceChild(newEl, element);
+	
+	$.ajax({
+		type: 'POST',
+		url: 'deleteFavorite.php',
+		data: {
+			'pkid': pkid
+		}
+	});
+}
+
+function unfaveList (pkid) {
+	var child = document.getElementById(String(pkid));
+	var parent = child.parentNode;
+	parent.removeChild(child);
 	
 	$.ajax({
 		type: 'POST',
