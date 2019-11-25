12700c507d9e71609a2754eb11537156389037a2
ande1426 <ande1426@pacificu.edu>
2019-11-24 20:21:16 -0800

    Added Admin functionality. Admins can make non-admin users into admins. The file with the most up to date NavBar (including the Admin Page) is showAdminPage

---
+++ PHP/queryUpdateUser.php
@@ -0,0 +1,13 @@
+<?php
+
+	require_once ('basicErrorHandling.php');
+	
+	function queryUpdateUser($dbh, $userID)
+	{
+		$sth = $dbh->prepare("UPDATE Users SET Admin = 1 WHERE
+					UID = :userIDNum");
+		$sth->bindValue(":userIDNum", $userID);
+		$sth->execute();
+	}
+
+?>
